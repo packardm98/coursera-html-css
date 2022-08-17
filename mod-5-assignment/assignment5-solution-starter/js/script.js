8fc91437c4341dc6f5a2ce3700dbe7b6023bde7c
packardm98 <109624538+packardm98@users.noreply.github.com>
2022-08-17 15:58:58 -0400

    Added solution for mod 5

--- mod-5-assignment/assignment5-solution-starter/js/script.js
+++ mod-5-assignment/assignment5-solution-starter/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -97,6 +97,13 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
+      var chosenCategoryShortName =
+          chooseRandomCategory(categories).short_name;
+      var homeHtmlToInsertIntoMainPage =
+          insertProperty(homeHTML,
+                         "randomCategoryShortName",
+                         "'"+chosenCategoryShortName+"'");
+          insertHtml("#main-content, homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
